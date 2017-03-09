278b54118572329f7996b990b96f5aed9d60f4f2
Marina Pereskokova <mstepanova@teide.maps.dev.yandex.net>
2017-03-09 11:32:03 +0300

    Additional csp check for opera 12

--- src/modules/system/supports/csp/csp.js
+++ src/modules/system/supports/csp/csp.js
@@ -1,7 +1,7 @@
 ym.modules.define('system.supports.csp', [], function (provide) {
     // Нельзя явно определить, поддерживается ли в браузере CSP.
-    // Поэтому проверяем косвенно по наличию объекта Blob.
+    // Поэтому проверяем косвенно по наличию объекта Blob (для старых IE) и URL (для Opera 12).
     provide({
-        isSupported: typeof Blob != 'undefined'
+        isSupported: (typeof Blob != 'undefined') && (typeof URL != 'undefined')
     });
 });
